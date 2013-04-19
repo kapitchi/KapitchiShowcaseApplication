4d00ee9cfa22173f1948b90dbc7c6bc5729572c1
Matus Zeman <mz@kapitchi.com>
2013-04-19 20:53:14 +0700

    composer loading moved after application.config.php so it can be used to init PHP settings or whatever

--- public/index.php
+++ public/index.php
@@ -8,11 +8,6 @@
 
 chdir(dirname(__DIR__));
 
-// Composer autoloading
-if (!include_once('vendor/autoload.php')) {
-    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
-}
-
 // Get application stack configuration
 $configuration = include 'config/application.config.php';
 
@@ -20,6 +15,11 @@ $configuration = include 'config/application.config.php';
 if(is_readable('config/application.local.php')) {
     $localConfig = include 'config/application.local.php';
     $configuration = array_merge_recursive($configuration, $localConfig);
+}
+
+// Composer autoloading
+if (!include_once('vendor/autoload.php')) {
+    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
 }
 
 // Run application
